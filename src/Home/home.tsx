67fcd747a51c674371f605f856133a336d4b5fc7
Evan Kim <89420301+wujehevankim@users.noreply.github.com>
2023-09-09 17:25:43 -0400

    custom highlight

--- src/Home/home.tsx
+++ src/Home/home.tsx
@@ -13,6 +13,9 @@ const codeSnippetStyle = {
     boxShadow: 'none',
   };
 
+
+
+
 // Define a custom style for the SyntaxHighlighter component
 const customSyntaxStyle = {
     ...dark,
@@ -20,6 +23,131 @@ const customSyntaxStyle = {
         textShadow: 'none', // Remove text shadow
     },
 };
+
+
+function highlightCodeSnippet(codeSnippet: string) {
+    const parts = codeSnippet.split("'''");
+  
+    const highlightedParts = parts.map((part, index) => {
+      if (index % 2 === 1) {
+        // Wrap the content between triple-quotes in a span with a yellow background
+        return (
+          <span key={index} style={{ color: 'rgb(197, 148, 124)' }}>
+            {'\u0027\u0027\u0027'}
+            {part}
+            {'\u0027\u0027\u0027'}
+          </span>
+        );
+      } else {
+        // Tokenize and highlight specific keywords in different colors
+        const keywordsToHighlight = {
+          'int': 'rgb(113, 197, 177)',
+          'def': 'rgb(93, 136, 183)',
+          'in': 'rgb(93, 136, 183)',
+          'while': 'rgb(179, 130, 180)',
+          'return': 'rgb(179, 130, 180)',
+          'print': 'rgb(179, 130, 180)',
+          'removeElement': 'green', // Highlight 'removeElement' in green
+          'greet': 'green', // Highlight 'greet' in green
+          'sum': 'green', // Highlight 'sum' in green
+        };
+        const tokenizedPart = part.split(/\b/); // Tokenize by word boundaries
+  
+        const highlightedText = tokenizedPart.map((word, wordIndex) => {
+          const color = keywordsToHighlight[word as keyof typeof keywordsToHighlight] || 'white';
+  
+          return (
+            <span key={wordIndex} style={{ color }}>
+              {word}
+            </span>
+          );
+        });
+  
+        return (
+          <span key={index}>
+            {highlightedText}
+          </span>
+        );
+      }
+    });
+  
+    return (
+        <pre style={{ lineHeight: '1.5' }}>{/* Add line-height style here */}
+          {highlightedParts}
+        </pre>
+      );
+  }
+
+  function highlightCodeSnippetWithInputs(codeSnippet: string) {
+    const parts = codeSnippet.split("'''");
+  
+    const highlightedParts = parts.map((part, index) => {
+      if (index % 2 === 1) {
+        // Wrap the content between triple-quotes in a span with a yellow background
+        return (
+          <span key={index} style={{ color: 'rgb(197, 148, 124)' }}>
+            {'\u0027\u0027\u0027'}
+            {part}
+            {'\u0027\u0027\u0027'}
+          </span>
+        );
+      } else {
+        // Tokenize and highlight specific keywords in different colors
+        const keywordsToHighlight = {
+          'int': 'rgb(113, 197, 177)',
+          'def': 'rgb(93, 136, 183)',
+          'in': 'rgb(93, 136, 183)',
+          'while': 'rgb(179, 130, 180)',
+          'return': 'rgb(179, 130, 180)',
+          'print': 'rgb(179, 130, 180)',
+          'removeElement': 'green', // Highlight 'removeElement' in green
+          'greet': 'green', // Highlight 'greet' in green
+          'sum': 'green', // Highlight 'sum' in green
+        };
+        const tokenizedPart = part.split(/\b/); // Tokenize by word boundaries
+  
+        const highlightedText = tokenizedPart.map((word, wordIndex) => {
+          const color = keywordsToHighlight[word as keyof typeof keywordsToHighlight] || 'white';
+  
+          // Replace specific words with input elements
+          if (['val', 'nums', 'removeElement'].includes(word)) {
+            return (
+              <input
+                key={wordIndex}
+                type="text"
+                style={{ color }}
+                placeholder={word}
+                // Handle input change here if needed
+              />
+            );
+          }
+  
+          return (
+            <span key={wordIndex} style={{ color }}>
+              {word}
+            </span>
+          );
+        });
+  
+        return (
+          <span key={index}>
+            {highlightedText}
+          </span>
+        );
+      }
+    });
+  
+    return (
+      <pre style={{ lineHeight: '1.5' }}>
+        {highlightedParts}
+      </pre>
+    );
+  }
+  
+  
+  
+  
+  
   
 function Home() {
     // Find the object with algoName === "sumABC"
@@ -93,9 +221,13 @@ function Home() {
     return (
         <>
             <h1>Hello World</h1>
-            <SyntaxHighlighter language="python" style={customSyntaxStyle} customStyle={codeSnippetStyle}>
-                {codeSnippet}
-            </SyntaxHighlighter>
+           
+            
+
+            {highlightCodeSnippet(codeSnippet)}
+
+            {highlightCodeSnippetWithInputs(codeSnippet)}
+            
             <button onClick={test3}>Fetch Data</button>
             <div>{apiMessage}</div>
         </>
