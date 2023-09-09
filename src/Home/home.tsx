44ddcbe22a095b7874d06dedb685ad43f9c005ae
Pascal Bell <pascal.bell@duke.edu>
2023-09-09 09:42:13 -0400

    api call sucessful

--- src/Home/home.tsx
+++ src/Home/home.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
-
-import pythonAlgosData from './pythonAlgos.json';
-
-
+import pythonAlgosData from '../../public/pythonAlgos.json';
 
 function Home() {
     const codeSnippet = pythonAlgosData.mappings[0]?.codeSnippet;
-
+    const [apiMessage, setApiMessage] = useState("");
+    
+    const test = async () => {
+        try {
+            const user = await fetch("/api/data").then(v => v.json());
+            console.log(user);
+            setApiMessage(user.message);
+        } catch (err) {
+            console.error(err);
+            setApiMessage("Something went wrong");
+        }
+    };
+    
     return (
         <>
             <h1>Hello World</h1>
             <pre>{codeSnippet}</pre>
+            <button onClick={test}>Fetch Data</button>
+            <div>{apiMessage}</div>
         </>
     );
 }
