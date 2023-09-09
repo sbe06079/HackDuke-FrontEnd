19ec8784b9a328ab0884c4cef790aa4cf77fd9ed
Evan Kim <89420301+wujehevankim@users.noreply.github.com>
2023-09-09 09:16:31 -0400

    evan first commit

--- src/Home/home.tsx
+++ src/Home/home.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./home.css";
 
+import pythonAlgosData from './pythonAlgos.json';
+
+
+
 function Home() {
-    return <>
-        Hello World
-    </>;
+    const codeSnippet = pythonAlgosData.mappings[0]?.codeSnippet;
+
+    return (
+        <>
+            <h1>Hello World</h1>
+            <pre>{codeSnippet}</pre>
+        </>
+    );
 }
 export default Home;
