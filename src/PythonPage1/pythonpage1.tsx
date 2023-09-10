c368f25cd5b48ec535fdd367413b455226fc699a
Pascal Bell <pascal.bell@duke.edu>
2023-09-09 21:36:59 -0400

    hint functionality

--- src/PythonPage1/pythonpage1.tsx
+++ src/PythonPage1/pythonpage1.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./pythonpage1.css";
+import { askChatGPT, translateText } from "../utils";
 
 function PythonPage1() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const lang = urlParams.get('lang')?.toString() ?? "EN";
+    const [speakingLanguage, setSpeakingLanguage] = useState(lang);
+    const [gptText, setGptText] = useState("");
+    const [translatedGptText, setTranslatedGptText] = useState("");
+    
+    const getHint = async () => {
+        askChatGPT("What is 1 + 11?")       // change question here
+            .then(response => {
+                    setGptText(response);
+                    translateText(response, speakingLanguage)
+                        .then(translation => setTranslatedGptText(translation))
+                        .catch(err => console.log(err));
+                })
+            .catch(err => console.log(err));
+    };
+
     return <div>
         <div className="header">
             <img id="logo" src="/FrameLogo.png" alt="Frame Logo" />;
@@ -13,10 +31,11 @@ function PythonPage1() {
         <div className="pythonContainer">
             <div className="questionBox">
                 <p id="elementsTitle"><span className="removeGreen">Remove</span> Elements</p>
-                <button>Get hint</button>
+                <button onClick={getHint}>Get hint</button>
             </div>
             <div className="questionBox"></div>
         </div>
+        <div>{translatedGptText}</div>
     </div>;
 }
 
